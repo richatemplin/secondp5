75f6362a38fa2c48c601fcbb683aaad2c8b3f6fc
richatemplin <143050257+richatemplin@users.noreply.github.com>
2023-08-29 16:38:40 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,14 @@
+var song;
+
+function preload(){
+  song = loadSound("");
+}
+
 function setup() {
-  createCanvas(600, 400);
-  background(100, 20, 100);
-  stroke(255,100);
+  createCanvas(400, 400);
+  song.play();
 }
 
 function draw() {
-
-  // ellipse
-  //fill(250, 200, 200);
-  //ellipse(mouseX, mouseY, 100, 100);
-  
-  // rectangle
-  fill(200, 250, 200);
-  rect(mouseX, mouseY, 50, 50);
-}
-
-// try mousePressed function 
-function mousePressed() {
-  background(100, 20, 100);
+  background(220);
 }
